7bcf51c5e696e4e5ade52054c9f1182528edf14a
Tudor Morar <tudor.morar@multiversx.com>
2025-12-09 15:02:57 +0200

    Routes structure working

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./components/Layout";
 import { AuthenticateUser } from "./pages/authenticate/AuthenticateUser";
-import { DynamicRoute } from "./components/DynamicRoute";
-import { ApprovalActions } from "./pages/approval/pages/ApprovalActions";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route element={<AuthenticateUser />}>
-          <Route index element={<></>} />
-          <Route
-            path="approval"
-            element={<DynamicRoute lookupPath="/approval" />}
-          >
-            <Route index element={<ApprovalActions />} />
-            <Route path="loader" element={<DynamicRoute />} />
-          </Route>
+        <Route element={<AuthenticateUser.Component />}>
+          {AuthenticateUser.Routes}
         </Route>
       </Route>
     </Routes>
--- src/pages/authenticate/AuthenticateUser.tsx
+++ src/pages/authenticate/AuthenticateUser.tsx
@@ -1,49 +1,62 @@
 import { useContext, useState } from "react";
-import { Outlet } from "react-router-dom";
 import { AppContext } from "../../context/context";
 import { useAwaitableNavigation } from "../../hooks/useAwaitableNavigation";
 import { ApprovalPage } from "../approval/ApprovalPage";
+import { Outlet, Route } from "react-router-dom";
+import { DynamicRoute } from "../../components/DynamicRoute";
+import { ApprovalActions } from "../approval/pages/ApprovalActions";
 
-export const AuthenticateUser = () => {
-  const { url } = useContext(AppContext);
-  const [approved, setApproved] = useState<boolean | null>(null);
-  const navigatePromise = useAwaitableNavigation();
+export const AuthenticateUser = {
+  Component: () => {
+    const { url } = useContext(AppContext);
+    const [approved, setApproved] = useState<boolean | null>(null);
+    const navigatePromise = useAwaitableNavigation();
 
-  const authenticateUser = async () => {
-    const response = await fetch(`${url}/todos/1`);
-    const data: { title: string } = await response.json();
+    const authenticateUser = async () => {
+      const response = await fetch(`${url}/todos/1`);
+      const data: { title: string } = await response.json();
 
-    // Register the component with props and navigate
-    const approved = await navigatePromise<boolean>(
-      "/approval",
-      <ApprovalPage text={data.title} />
+      // Register the component with props and navigate
+      const approved = await navigatePromise<boolean>(
+        "/approval",
+        <ApprovalPage text={data.title} />
+      );
+
+      setApproved(approved);
+    };
+
+    const reset = () => {
+      setApproved(null);
+    };
+
+    return (
+      <div className="card">
+        {approved === true && (
+          <>
+            <p>User approved</p>
+            <button onClick={reset}>Reset</button>
+          </>
+        )}
+        {approved === false && (
+          <>
+            <p>User rejected</p>
+            <button onClick={reset}>Reset</button>
+          </>
+        )}
+        {approved === null && (
+          <button onClick={authenticateUser}>Authenticate User</button>
+        )}
+        <Outlet />
+      </div>
     );
-
-    setApproved(approved);
-  };
-
-  const reset = () => {
-    setApproved(null);
-  };
-
-  return (
-    <div className="card">
-      {approved === true && (
-        <>
-          <p>User approved</p>
-          <button onClick={reset}>Reset</button>
-        </>
-      )}
-      {approved === false && (
-        <>
-          <p>User rejected</p>
-          <button onClick={reset}>Reset</button>
-        </>
-      )}
-      {approved === null && (
-        <button onClick={authenticateUser}>Authenticate User</button>
-      )}
-      <Outlet />
-    </div>
-  );
+  },
+  Routes: (
+    <>
+      <Route index element={<></>} />
+      <Route path="approval" element={<DynamicRoute lookupPath="/approval" />}>
+        <Route index element={<ApprovalActions />} />
+        <Route path="loader" element={<DynamicRoute />} />
+      </Route>
+    </>
+  ),
 };
